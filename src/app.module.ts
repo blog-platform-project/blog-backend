21f5b30cb5d8e04eb7196e7cb2934dfef8e18e22
daviddlhz <vidda1959@gmail.com>
2024-05-28 12:48:38 -0500

    base of project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { RepliesModule } from './replies/replies.module';
+import { ConnectionsModule } from './connections/connections.module';
+import { LikesModule } from './likes/likes.module';
+import { FeedModule } from './feed/feed.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+    RepliesModule,
+    ConnectionsModule,
+    LikesModule,
+    FeedModule,
+    SearchModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
